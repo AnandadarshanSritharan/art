180e4f4637413d1394f3a71b897ba063e930ef46
Anandadarshan <anandadarshan99@gmail.com>
2025-12-06 19:26:50 +0530

    config file change

--- client/public/config.js
+++ client/public/config.js
@@ -1,4 +1,10 @@
+// public/config.js
+
+// Detect if we are in production (deployed on Vercel) or local
+const isProduction = window.location.hostname !== "localhost";
 
 window.appConfig = {
-    API_URL: "http://localhost:5000"
+    API_URL: isProduction
+        ? "https://backend-3jwwdw9og-anandadarshans-projects.vercel.app"
+        : "http://localhost:5000" // your local backend
 };
