485ba33c2c163a8b4be8cb0979a39ce6a4a07226
Anandadarshan <anandadarshan99@gmail.com>
2025-12-07 17:05:14 +0530

    api path

--- client/src/utils/config.ts
+++ client/src/utils/config.ts
@@ -1,7 +1,4 @@
 
 export const getApiUrl = (): string => {
-    if (window.location.hostname === 'localhost') {
-        return 'http://localhost:5000';
-    }
-    return ''; // Relative path for production
+    return (window as any).appConfig?.API_URL || 'http://localhost:5000';
 };
